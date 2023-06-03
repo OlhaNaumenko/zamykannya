8df265560a31711c41d8fe574d10bf745bdc561d
OlhaNaumenko <90687694+OlhaNaumenko@users.noreply.github.com>
2023-06-03 08:25:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+# def sayUserHello(user):  # охоплююча функція
+#     msg = 'hello, '+ user
+#     def showMsg():   # вкладена функція
+#         nonlocal msg
+#         msg = 'student'
+#         print(msg+', lets stert')
+#     showMsg()
+#     print(msg)
+#
+# sayUserHello('admin')
+
+"""def sayHello(user):
+    msg = 'Hi, '+user
+    def show():
+        print(msg+' and world')
+    return show
+
+case1=sayHello('admin')
+case1()"""
+
+# def doExercise1(var1):
+#     var2 = 5
+#     def doExercise2(var2):
+#         return var1**var2
+#     return doExercise2
+# case1=doExercise1(2)
+# print(case1(5))
+# print(case1(10))
+
+
+
+
+
+
